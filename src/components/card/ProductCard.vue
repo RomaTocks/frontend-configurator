c0ae7d78f0a047ebf5ecbd2e7a29a9cb41c823df
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-04 00:03:39 +0300

    Added product card.

---
+++ src/components/card/ProductCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div style="max-width: 1385px" class="mb-3">
+    <v-card max-width="1385px" class="d-flex pa-4 align-center">
+      <v-container>
+        <v-row justify="start">
+          <h4>{{ product.name }}</h4>
+        </v-row>
+        <v-row justify="center">
+          <v-col class="align-self-center" cols="5">
+            <v-img
+              max-width="200px"
+              contain
+              :lazy-src="product.images.header"
+              :src="product.images.header"
+            ></v-img>
+          </v-col>
+          <v-col>
+            <v-row class="mb-2" justify="end" align="center">
+              <v-rating
+                :value=product.reviews.rating/10
+                background-color="grey darken-3"
+                color="yellow accent-4"
+                half-increments
+                readonly
+                size="18"
+              ></v-rating>
+              <h4>{{product.reviews.rating/10}}, {{product.reviews.count}} отзывов</h4>
+            </v-row>
+            <v-row>
+              {{ product.description }}
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-row justify="space-between" align="end">
+          <v-col align="center">
+            <v-row align="center" >
+              <h4 class="mr-2">от {{ product.prices.price_min.value }} руб.</h4><v-btn color="primary">{{ product.prices.offers.count }} предложений</v-btn>
+            </v-row>
+          </v-col>
+          <v-col>
+            <v-row align="end" justify="end">
+              <v-btn :color="current === product.id ? 'red' : 'blue'" @click="card(product.id)">{{ current === product.id ? "В сборке" : 'В сборку' }}</v-btn>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+name: "ProductCard",
+  props:{
+    card:Function,
+    product:Object,
+    current:String
+  },
+  methods : {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
