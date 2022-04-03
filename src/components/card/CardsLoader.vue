bc2bafdf7469a7e0743ed8ce5bfd09d3d0b6d35b
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-04 00:05:34 +0300

    Added cards loader.

---
+++ src/components/card/CardsLoader.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row v-if="loading">
+    <v-col>
+      <v-card :key="n" v-for="n in count" class="mb-4">
+        <v-skeleton-loader
+          type="article, actions"
+        ></v-skeleton-loader>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+name: "CardsLoader",
+  props: {
+    loading : Boolean,
+    count : Number
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
