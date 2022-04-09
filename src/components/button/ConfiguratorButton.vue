3c74799bd86be8110fe708e3068f6ffb0124f292
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-10 02:07:41 +0300

    Added configuration button.

---
+++ src/components/button/ConfiguratorButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn :loading="configLoader" fab dark large color="blue accent-4" fixed right bottom
+         @click="configDialog">
+    <v-icon dark>mdi-desktop-classic</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "ConfiguratorButton",
+  methods: {
+    configDialog() {
+      this.$store.commit('dialog');
+    },
+  },
+  computed: {
+    configLoader() {
+      return this.$store.getters.getConfigLoader;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
