9ffe727385e715de6fbbe510c8475cf8d6c33230
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-10 02:04:37 +0300

    Added header.

---
+++ src/components/app/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NavigationDrawer :drawer="drawer"/>
+    <v-app-bar
+      sticky
+      color="blue accent-4"
+      dark
+      max-height="64px"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <img alt="logo" width="25" src="@/assets/logo.png" class="mr-2">
+      <v-toolbar-title>Онлайн-конфигуратор</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <v-avatar color="white" size="45">
+        <span class="black--text text-p">RT</span>
+      </v-avatar>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import NavigationDrawer from "@/components/app/navigation/NavigationDrawer";
+export default {
+name: "AppHeader",
+  components: {NavigationDrawer},
+  data: () =>({
+    drawer : false
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
