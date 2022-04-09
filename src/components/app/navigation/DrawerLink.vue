24fdfd1413b534ca183eef7ca4291af814a9c1a6
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-10 02:08:48 +0300

    Added drawer link.

---
+++ src/components/app/navigation/DrawerLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-list-item>
+    <v-list-item-icon>
+      <v-icon>mdi-home</v-icon>
+    </v-list-item-icon>
+    <v-list-item-title><router-link class="text-decoration-none" :to="to">{{name}}</router-link></v-list-item-title>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props : {
+    name : String,
+    to : Object,
+  },
+name: "DrawerLink"
+}
+</script>
+
+<style scoped>
+
+</style>
