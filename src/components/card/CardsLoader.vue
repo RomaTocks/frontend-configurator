3fb6d4567ff2056d64596ae85f304c8ca5b30dea
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-10 02:05:06 +0300

    Update cards loader.

--- src/components/card/CardsLoader.vue
+++ src/components/card/CardsLoader.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-row v-if="loading">
-    <v-col>
-      <v-card :key="n" v-for="n in count" class="mb-4">
-        <v-skeleton-loader
-          type="article, actions"
-        ></v-skeleton-loader>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container style="max-width: 1385px">
+    <v-row  v-if="loading">
+      <v-col>
+        <v-card :key="n" v-for="n in count" class="mb-4">
+          <v-skeleton-loader
+              type="article, actions"
+          ></v-skeleton-loader>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
