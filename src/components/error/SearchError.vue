da3c708f4da3f78a3c3598fa72467f77e64c4569
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-04-10 18:32:42 +0300

    Added search error.

---
+++ src/components/error/SearchError.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex justify-center align-center" style="height: 500px">
+    <div>
+      <v-alert
+        text
+        prominent
+        type="info"
+        icon="mdi-cloud-alert"
+      >
+        Ой, мы ничего не нашли по вашему запросу. Попробуйте поменять фильтры!
+      </v-alert>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "SearchError"
+}
+</script>
+
+<style scoped>
+
+</style>
