e293d30ea94ee2bb63567cc2a3925f983bb1bae1
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-05-18 23:40:38 +0300

    Added pagination to product's list.

---
+++ src/components/products/Pagination.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row justify="center">
+    <v-pagination
+      v-model="page"
+      class="my-4"
+      :length="processors.pages"
+    ></v-pagination>
+  </v-row>
+</template>
+
+<script>
+export default {
+name: "Pagination"
+}
+</script>
+
+<style scoped>
+
+</style>
