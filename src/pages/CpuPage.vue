e745f3b6e421c3400355f559985fb017a6ae1dc3
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-06-04 00:20:09 +0300

    Added elements pages.

---
+++ src/pages/CpuPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <ComponentsWrapper :header="true" :configuration="true" :footer="true">
+    <ProductsPage :path="path"/>
+  </ComponentsWrapper>
+</template>
+
+<script>
+/* eslint-disable vue/no-deprecated-destroyed-lifecycle */
+import ProductsPage from "@/components/products/ProductsContainer";
+import ComponentsWrapper from "@/components/wrapper/ComponentsWrapper";
+export default {
+name: "CpuPage",
+  props: {
+    path : String,
+  },
+  methods:{
+    setController() {
+      this.$store.commit('setController')
+    }
+  },
+  beforeDestroy() {
+    this.setController();
+  },
+  components: {ComponentsWrapper, ProductsPage}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/MotherboardPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <ComponentsWrapper :header="true" :configuration="true" :footer="true">
+    <ProductsPage :path="path"/>
+  </ComponentsWrapper>
+</template>
+
+<script>
+/* eslint-disable vue/no-deprecated-destroyed-lifecycle */
+import ProductsPage from "@/components/products/ProductsContainer";
+import ComponentsWrapper from "@/components/wrapper/ComponentsWrapper";
+
+export default {
+name: "MotherboardPage",
+  props: {
+    path : String,
+  },
+  methods:{
+    setController() {
+      this.$store.commit('setController')
+    }
+  },
+  beforeDestroy() {
+    this.setController();
+  },
+  components: {ComponentsWrapper, ProductsPage}
+}
+</script>
+
+<style scoped>
+
+</style>
