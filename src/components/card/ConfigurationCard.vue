b2eeeb7d1a27a6447e1be61c01ebfcd4c34b6fc9
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-06-05 01:01:32 +0300

    Update ui of configuration card.

--- src/components/card/ConfigurationCard.vue
+++ src/components/card/ConfigurationCard.vue
@@ -1,42 +1,32 @@
 <template>
-  <div v-if="product !== null || product !== undefined" style="max-width: 1385px" class="mb-3">
+  <div v-if="product !== null" style="max-width: 1385px" class="mb-3">
     <v-card max-width="1385px" class="d-flex pa-4 align-center">
       <v-container>
-        <v-row justify="start">
-          <h4>{{ product.name }}</h4>
+        <v-row class="pb-2">
+          <v-col class="pa-0 d-flex justify-start">
+            <router-link class="text-decoration-none" style="color: black" :to="'/' + changePath + '/' + product.id"><h4>{{ product.name }}</h4></router-link>
+          </v-col>
+          <v-col class="pa-0 d-flex justify-end">
+            <v-btn @click="removeCurrent" icon><v-icon>mdi-delete</v-icon></v-btn>
+          </v-col>
         </v-row>
         <v-row justify="center">
-          <v-col class="align-self-center" cols="5">
-            <v-img v-if="product.images.header !== undefined"
-              max-width="200px"
+          <v-col class=" pa-0 align-self-start">
+            <v-img v-if="product.images.header !== undefined || product.images.header !== null"
+              width="100px"
               contain
               :lazy-src="product.images.header"
               :src="product.images.header"
             ></v-img>
           </v-col>
-          <v-col>
-            <v-row class="mb-2" justify="end" align="center">
-              <v-rating
-                :value=product.reviews.rating/10
-                background-color="grey darken-3"
-                color="yellow accent-4"
-                half-increments
-                readonly
-                empty-icon="mdi-star-outline"
-                full-icon="mdi-star"
-                half-icon="mdi-star-half-full"
-                size="18"
-              ></v-rating>
-              <h4>{{product.reviews.rating/10}} | {{product.reviews.count}}</h4>
-            </v-row>
-            <v-row>
-              {{ product.description }}
-            </v-row>
-          </v-col>
-        </v-row>
-        <v-row justify="space-between" align="end">
-          <h4 class="mr-2">от {{ product.prices.price_min.value }} руб.</h4>
-          <v-btn color="primary">{{ product.prices.offers.count }} предложений</v-btn>
+            <v-col class="d-flex flex-column">
+              <v-row>
+                  <p class="text-justify">{{ product.description }}</p>
+              </v-row>
+              <v-row class="d-flex align-self-end justify-end">
+                  <v-btn @click="changeProduct">Изменить</v-btn>
+              </v-row>
+            </v-col>
         </v-row>
       </v-container>
     </v-card>
